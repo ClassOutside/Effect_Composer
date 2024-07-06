b23ebd8d5a5690ea8b197f40b95a26698669666a
ClassOutside <132165158+ClassOutside@users.noreply.github.com>
2024-07-06 14:02:07 -0400

    Delete interface directory

--- interface/EffectComposerUIList.py
+++
@@ -1,5 +0,0 @@
-import bpy
-
-class EffectComposerUIList(bpy.types.UIList):
-    def draw_item(self, context, layout, data, item, icon, active_data, active_propname):
-        layout.label(text=item.name)
